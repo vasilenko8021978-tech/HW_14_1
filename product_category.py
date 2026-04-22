34dd6570c1e1e6739c8d3e0253f588b9a3ac5e2b
dr.Alexander Vasilenko <vasilenko8021978@gmail.com>
2026-04-22 08:12:25 +0300

    Добавил ридми

--- product_category.py
+++ product_category.py
@@ -10,9 +10,15 @@ class Product:
 
 
 class Category:
+    # Атрибуты класса — именно такие имена, как в main.py
+    category_count = 0
+    product_count = 0
 
     def __init__(self, name: str, description: str, products: List[Product]):
         self.name = name
         self.description = description
         self.products = products
 
+        # Обновляем счётчики класса
+        Category.category_count += 1
+        Category.product_count += len(products)
